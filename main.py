851e632d3cae2f7145d26aa4c6a2453121f1e94e
VakhtangTsintsadze777 <vaxo19902010@gmail.com>
2025-02-10 12:24:07 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+names = ["dato", "nika", "gio"]
+print(names[0])
+print(names[1])
+print(names[2])
+
+print(f"hi {names[0]}")
+print(f"hi {names[1]}")
+print(f"hi {names[2]}")
+
+transportation = ["bicycle", "bmw", "plane"]
+
+print("I would like to own a " + transportation[0])
+print("I would like to own a " + transportation[1])
+print("I would like to own a " + transportation[2])
+
+invite_list = ["dato", "nika", "gio"]
+
+print(f"i want to invite {invite_list[0]}")
+print(f"i want to invite {invite_list[1]}")
+print(f"i want to invite {invite_list[2]}")
+
